2f2d714d9591a937ca9898d2871f8651180555cb
Austin Miller <austinmmiller00@gmail.com>
2026-08-02 15:27:01 +0930

    Initial project setup

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,25 @@
+function HomePage() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#F7F7F7",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "64px",
+          fontWeight: 600,
+          letterSpacing: "-0.05em",
+        }}
+      >
+        Via
+      </h1>
+    </div>
+  );
+}
+
+export default HomePage;
