7fa6c3871413135debc2eabd5e5737b363728ab0
Austin Miller <austinmmiller00@gmail.com>
2026-08-02 16:15:16 +0930

    Add clean Voyager basemap

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -4,18 +4,18 @@ import "leaflet/dist/leaflet.css";
 function Map() {
   return (
     <MapContainer
-  center={[41.3874, 2.1686]}
-  zoom={9}
-  minZoom={5}
-  maxZoom={10}
-  zoomSnap={0.1}
-  zoomDelta={0.1}
-  wheelPxPerZoomLevel={2000}
-  style={{ width: "100%", height: "100%" }}
->
+      center={[41.3874, 2.1686]}
+      zoom={9}
+      minZoom={5}
+      maxZoom={10}
+      zoomSnap={0.1}
+      zoomDelta={0.1}
+      wheelPxPerZoomLevel={2000}
+      style={{ width: "100%", height: "100%" }}
+    >
       <TileLayer
-        attribution="© OpenStreetMap contributors"
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors &copy; CARTO"
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png"
       />
     </MapContainer>
   );
